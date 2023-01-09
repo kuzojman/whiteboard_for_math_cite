a92a8d1e14ed050785c1588e26295dbc4a567d00
meteo <f2re@yandex.ru>
2023-01-09 22:10:29 +0300

    merge fix

--- server_part.js
+++ server_part.js
@@ -793,3 +793,129 @@ charactersLength));
  }
  return result;
 }
+
+/**
+ * Конвертируем файл в массив картинок и загружаем в облако
+ * @param {*} file_ 
+ * @socket_id - айди доски для формирования пути в облаке
+ * @uid_ - айди папки с файлами
+ */
+async function convertPDFToImages(file_, uid_, socket_id_){
+  let fileContent = fs.readFileSync(file_);
+  let pdf_params = await getPdfFormatInfo(fileContent);
+
+  let opts = {
+    format: 'jpeg',
+    saveFilename: "page",
+    savePath: path.dirname(file_),
+    density: 300,
+    quality: 78,
+  }
+
+  // задаем правильный размер pdf
+  if ( pdf_params ){
+    opts['height'] = pdf_params.finalHeight;
+    opts['width'] = pdf_params.finalWidth;
+  }
+
+  let result = await fromPath(file_, opts).bulk(-1, false);
+
+  if (result){
+    fs.unlinkSync(file_);
+    // console.log("saveImagesFromPathToCloud");
+    return saveImagesFromPathToCloud(uid_, socket_id_);
+  }
+  return [];
+}
+
+/**
+ * Конвертируем презентацию в массив картинок и загружаем в облако
+ * @param {*} file_ 
+ */
+function convertPPTToImages(file_,uid_, socket_id_){
+  return unoconv.convert(file_, { output: file_+'.pdf', format: 'pdf' })  // or format: 'html'
+    .then(result => {
+      console.log(`File save at ${result}`);
+      return result;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+  // const commandOffice = `"${office}" --headless --convert-to pdf --outdir "${path.dirname(file_)}" "${file_}"`;
+  
+  // console.log(result);
+  return true;
+}
+
+/**
+ * сохраняем картики из папки в облако и удаляем их из папки
+ * @param {String} uid_ айди папки
+ * @returns 
+ */
+async function saveImagesFromPathToCloud( uid_, socket_id_ ){
+  let images = [];
+  var promises = [];
+  let dir = "./uploaded/"+uid_+"/";
+  // console.log("saveImagesFromPathToCloud",dir,fs.existsSync(dir));
+  
+  if (fs.existsSync(dir)){
+    // options is optional
+    
+    let files = glob.sync(dir+"*.jpeg");
+    
+    // console.log(files);
+    if (files) {
+      var i = 0;
+      for (const file of files) {
+        // console.log(file, path.basename(file));
+        // promises.push(
+        let fileContent = fs.readFileSync(file);
+        let one = await  AWSCloud.upload({
+            file: fileContent, // файл
+            path: 'images/'+socket_id_+'/'+uid_,
+            fileName: path.basename(file),
+            type: "image/jpeg"
+          }).then( (data)=> {
+            fs.unlinkSync(file);
+            // console.log(data.Location);
+            return data.Location;
+          } )
+        images.push(one);
+        // );
+  
+        i++;
+      };
+      // return Promise.allSettled(promises).then((a) => {
+      //   console.log("promisec");
+      //   fs.rmSync(dir, { recursive: true, force: true });
+      //   return a;
+      // });
+    }
+    
+    
+    fs.rmSync(dir, { recursive: true, force: true });
+  }
+  
+  return images;
+}
+
+/** returns size and resolution of the pdf */
+async function getPdfFormatInfo(dataBuffer){
+  const pdfDocument = await getDocument({ data: dataBuffer }).promise;
+  const page = await pdfDocument.getPage(1);
+  const viewport = page.getViewport({ scale: 1 });
+
+  const width = Math.floor(viewport.width);
+  const height = Math.floor(viewport.height);
+  const finalHeight = 800;
+  const finalWidth = (finalHeight / height) * width;
+
+  return {
+    numPages: pdfDocument.numPages,
+    width,
+    height,
+    finalWidth,
+    finalHeight,
+  };
+}
